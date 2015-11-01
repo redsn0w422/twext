0676baa9857a7ae8e41abefc13ba193800e88b8d
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-10-31 23:26:45 -0400

    basic text/recieve works

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var config = require('./config');
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var request = require('request');
+var client = require('twilio')(config.accountSid, config.authToken);
+
 
 var app = express();
 
@@ -24,6 +31,57 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+// UNCOMMENT TO LOOP
+// setTimeout(MAIN_LOOP, 3000);
+
+var states = {};
+
+MAIN_LOOP = function(){
+  // checkStream("tsm_bjergsen", "6303037034", checked);
+  // checkStream("dreamhackcs", "6307294437", checked);
+
+
+  // client.messages.create({
+  //   body: "if you had two penises, would one of them be mine?",
+  //   to: "6308158668",
+  //   from: "+13313056064"
+  // });
+}
+
+checkStream = function(channel, number, callback) {
+  var url = "https://api.twitch.tv/kraken/streams/" + channel;
+  request(url, function (error, response, body) {
+    json = JSON.parse(body);
+    if (json.stream) {
+      // stream is offline
+      callback(true, number, channel);
+    }
+    else {
+      // stream is online
+      callback(false, number, channel);
+    }
+  });
+}
+
+checked = function(live, number, channel){
+  console.log(live + number + channel);
+  str = live ? " is ":" is not ";
+  client.messages.create({
+    body: channel + str + "live.",
+    to: number,
+    from: "+13313056064"
+  });
+}
+
+MAIN_LOOP();
+
+app.post('/incoming', function(req, res, next){
+  var message = req.body.Body;
+  var from = req.body.From;
+  console.log("\t" + message + "\n\tfrom: " + from);
+  checkStream(message, from, checked);
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,7 +114,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000);
-
+app.listen(3000, function() {
+  console.log('Express server listening on port %d in %s mode', 3000, app.get('env'));
+});
 
 module.exports = app;
