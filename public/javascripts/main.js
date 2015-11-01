9d239634d743274b669ded6425ddbc1549a159bf
Yasha Mostofi <yasha.mostofi@gmail.com>
2015-11-01 11:33:59 -0500

    alert added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   $("#submit").click(function(){
     $.post( "/signup", {number: $("#phone").val()} );
+    alert("Thanks! Check your phone for the next step.");
+    $("#form").toggle();
   });
 });
